6daec66fcbdf9f7c2360b0ccda21d19cf4d49d84
ieeemma <emma.freeman@protonmail.com>
2025-07-17 14:21:25 +0100

    Add drag detection

--- android/app/src/main/kotlin/com/example/sensor_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sensor_app/MainActivity.kt
@@ -8,6 +8,8 @@ import android.hardware.SensorManager
 import android.os.PowerManager
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -24,11 +26,17 @@ class MainActivity : FlutterActivity(), SensorEventListener {
     private var sensor: Sensor? = null
     private lateinit var udpThread: Thread
     private lateinit var wakeLock: PowerManager.WakeLock
+    private lateinit var gestureDetector: GestureDetector
 
     @Volatile
     private var ip: String? = null
+
     @Volatile
-    private var rotation: RotationVector = RotationVector(0f, 0f, 0f)
+    private var rotation: Rotation = Rotation(0f, 0f, 0f)
+    @Volatile
+    private var drag: Drag? = null
+    @Volatile
+    private var release: Boolean = false
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -36,6 +44,12 @@ class MainActivity : FlutterActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "SensorApp::WakeLock")
+        wakeLock.acquire()
+
+        gestureDetector = GestureDetector(this, GestureListener())
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "gyro_udp_channel")
             .setMethodCallHandler { call, result ->
@@ -51,10 +65,24 @@ class MainActivity : FlutterActivity(), SensorEventListener {
                 while (true) {
                     Thread.sleep(1)
                     if (ip == null) continue;
-                    val d = osc("/rotation", arrayOf(rotation.x, rotation.y, rotation.z))
-                    val packet = DatagramPacket(d, d.size, InetAddress.getByName(ip), 8000)
                     try {
+                        var d = osc("/rotation", arrayOf(rotation.x, rotation.y, rotation.z))
+                        var packet = DatagramPacket(d, d.size, InetAddress.getByName(ip), 8000)
                         udpSocket?.send(packet)
+                        
+                        if (drag != null) {
+                            d = osc("/drag", arrayOf(drag!!.sx, drag!!.sy, drag!!.ex, drag!!.ey))
+                            packet = DatagramPacket(d, d.size, InetAddress.getByName(ip), 8000)
+                            udpSocket?.send(packet)
+                            drag = null
+                        }
+                        
+                        if (release) {
+                            d = osc("/release", arrayOf())
+                            packet = DatagramPacket(d, d.size, InetAddress.getByName(ip), 8000)
+                            udpSocket?.send(packet)
+                            release = false
+                        }
                     } catch (e: Exception) {
                         Log.e("UDP", "Send failed: ${e}")
                     }
@@ -71,17 +99,22 @@ class MainActivity : FlutterActivity(), SensorEventListener {
         SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
         val orientation = FloatArray(3)
         SensorManager.getOrientation(rotationMatrix, orientation)
-        rotation = RotationVector(orientation[0], orientation[1], orientation[2])
+        rotation = Rotation(orientation[0], orientation[1], orientation[2])
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.getAction() == MotionEvent.ACTION_UP) release = true
+        if (gestureDetector.onTouchEvent(event)) return true
+        return super.dispatchTouchEvent(event)
+    }
 
-        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-        wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "SensorApp::WakeLock")
-        wakeLock.acquire()
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(e1: MotionEvent?, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+            drag = Drag(e1?.x ?: e2.x, e1?.y ?: e2.y, e2.x, e2.y)
+            return true
+        }
     }
 
     override fun onDestroy() {
@@ -96,7 +129,7 @@ class MainActivity : FlutterActivity(), SensorEventListener {
         val out = ByteArrayOutputStream()
 
         oscString(out, addr)
-        oscString(out, args.map { oscType(it) }.joinToString(""))
+        oscString(out, "," + args.map { oscType(it) }.joinToString(""))
         for (arg in args) when (arg) {
             is String -> oscString(out, arg)
             is Float -> out.write(ByteBuffer.allocate(4).putFloat(arg).array())
@@ -123,4 +156,6 @@ class MainActivity : FlutterActivity(), SensorEventListener {
     }
 }
 
-data class RotationVector(val x: Float, val y: Float, val z: Float)
+data class Rotation(val x: Float, val y: Float, val z: Float)
+
+data class Drag(val sx: Float, val sy: Float, val ex: Float, val ey: Float)
